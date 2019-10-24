0a46c0db2ae87dee2d0c368eeb0b246d7635e9bd
ACFC <admin@unethicalimages.us>
2019-10-23 22:46:09 -0400

    Added config-template.json and cleaned up coinflip & dice

--- commands/dice.js
+++ commands/dice.js
@@ -1,4 +1,4 @@
 exports.run = (client, message, args) => {
-    let dice = Math.floor(Math.random() * 6) + 1
-    message.channel.send("The dice has rolled a " + dice + "!")
+	let dice = Math.floor(Math.random() * 6) + 1
+	message.channel.send("The dice has rolled a " + dice + "!")
 }
