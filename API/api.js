4d7d1e96fea7a477407fcfc60ec2c7b6f5ab75e1
ACFC <admin@unethicalimages.us>
2019-10-26 02:29:22 -0400

    Got the api working

--- API/api.js
+++ API/api.js
@@ -20,19 +20,59 @@ app.get('/info', function(req, res) {
 });
 
 app.get('/bans', function(req, res) {
-
+    connection.query('SELECT * FROM bans WHERE userid = ? ', [req.query.userid], function(err, rows, result) {
+        if (req.query.userid === ``) {
+            res.send(`[{"success":"false","error":"no userid provided"},"errorcode":"1100"}]`)
+        } else {
+            if (!rows.length) {
+                res.send(`[{"success":"false","error":"no bans"},"errorcode":"1300"}]`)
+            } else {
+                res.send(`[{"userid":"${rows[0].userid}","modid":"${rows[0].modid}","banid":"${rows[0].banid}","reason":"${rows[0].reason}","date":"${rows[0].date}"}]`)
+            }
+        }
+    });
 });
 
 app.get('/warns', function(req, res) {
-
+    connection.query('SELECT * FROM warnings WHERE userid = ?', [req.query.userid], function(err, rows, result) {
+        if (req.query.userid === ``) {
+            res.send(`[{"success":"false","error":"no userid provided"},"errorcode":"1100"}]`)
+        } else {
+            if (!rows.length) {
+                res.send(`[{"success":"false","error":"no warns"},"errorcode":"1300"}]`)
+            } else {
+                res.send(`[{"userid":"${rows[0].userid}","modid":"${rows[0].modid}","warningid":"${rows[0].warningid}","reason":"${rows[0].reason}","date":"${rows[0].date}"}]`)
+            }
+        }
+    });
 });
 
 app.get('/kicks', function(req, res) {
-
+    connection.query('SELECT * FROM kicks WHERE userid = ?', [req.query.userid], function(err, rows, result) {
+        if (req.query.userid === ``) {
+            res.send(`[{"success":"false","error":"no userid provided"},"errorcode":"1100"}]`)
+        } else {
+            if (!rows.length) {
+                res.send(`[{"success":"false","error":"no kicks"},"errorcode":"1300"}]`)
+            } else {
+                res.send(`[{"userid":"${rows[0].userid}","modid":"${rows[0].modid}","kickid":"${rows[0].kickid}","reason":"${rows[0].reason}","date":"${rows[0].date}"}]`)
+            }
+        }
+    });
 });
 
 app.get('/mutes', function(req, res) {
-
+    connection.query('SELECT * FROM mutes WHERE userid = ?', [req.query.userid], function(err, rows, result) {
+        if (req.query.userid === ``) {
+            res.send(`[{"success":"false","error":"no userid provided"},"errorcode":"1100"}]`)
+        } else {
+            if (!rows.length) {
+                res.send(`[{"success":"false","error":"no mutes"},"errorcode":"1300"}]`)
+            } else {
+                res.send(`[{"userid":"${rows[0].userid}","modid":"${rows[0].modid}","muteid":"${rows[0].muteid}","reason":"${rows[0].reason}","date":"${rows[0].date}"}]`)
+            }
+        }
+    });
 });
 
 
