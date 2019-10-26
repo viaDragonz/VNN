3a2d8a219b1b2b154b366d503f698ecd0f14349e
Jota <jotarjotinha@gmail.com>
2019-10-26 20:34:21 -0300

    added tyler images

--- commands/tyler.js
+++ commands/tyler.js
@@ -114,7 +114,10 @@ exports.run = (client, message, args) => {
         "https://cdn.discordapp.com/attachments/258324481074921472/637477191076282414/tyler_OHNO.png",
         "https://cdn.discordapp.com/attachments/258324481074921472/637477782611296275/TYLER_demo.png",
         "https://cdn.discordapp.com/attachments/258324481074921472/637474782434164745/TYLERbeta.png",
-        "https://cdn.discordapp.com/attachments/258324481074921472/630602060231868446/cascade.jpg"
+        "https://cdn.discordapp.com/attachments/258324481074921472/630602060231868446/cascade.jpg",
+        "https://cdn.discordapp.com/attachments/258324481074921472/637790866320130048/unknown.png",
+        "https://cdn.discordapp.com/attachments/258324481074921472/637792578313650216/unknown.png",
+        "https://cdn.discordapp.com/attachments/258324481074921472/637789151529271318/unknown.png"
     ];
     message.channel.send("", {
         file: `${tyler[Math.floor(Math.random() * tyler.length)]}`
