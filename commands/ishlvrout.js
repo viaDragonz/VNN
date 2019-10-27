2ae8087fb2100cb1ca0614fe9b65d6c9dab381a3
toddhowardguild <53285119+toddhowardguild@users.noreply.github.com>
2019-10-27 16:04:46 +1030

    Update ishlvrout
    If it's going to @, might as well make it use different punctuation.

--- commands/ishlvrout.js
+++ commands/ishlvrout.js
@@ -11,7 +11,7 @@ exports.run = (client, message, args) => {
     if (talkedRecently.has(message.author.id)) {
         message.channel.send("Wait 15 seconds before trying this again. - " + message.author);
     } else {
-        message.reply(`Not yet. Try again sooner or layer :(`)
+        message.reply(`not yet... Try again sooner or layer :(`)
         talkedRecently.add(message.author.id);
         setTimeout(() => {
             talkedRecently.delete(message.author.id);
