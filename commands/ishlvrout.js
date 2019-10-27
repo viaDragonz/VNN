03931dd599a794ed5c2e041a2aa7bdcce2166ffe
toddhowardguild <53285119+toddhowardguild@users.noreply.github.com>
2019-10-27 15:57:51 +1030

    Is HLVR out?
    "No... not yet. I'm not strong enough."
    
    May/may not be totally broken.

---
+++ commands/ishlvrout.js
@@ -0,0 +1,20 @@
+const config = require("../config.json");
+const Sentry = require('@sentry/node');
+Sentry.init({
+    dsn: `${config.dsn}`
+});
+
+
+const talkedRecently = new Set();
+
+exports.run = (client, message, args) => {
+    if (talkedRecently.has(message.author.id)) {
+        message.channel.send("Wait 15 seconds before trying this again. - " + message.author);
+    } else {
+        message.reply(`Not yet. Try again sooner or layer :(`)
+        talkedRecently.add(message.author.id);
+        setTimeout(() => {
+            talkedRecently.delete(message.author.id);
+        }, 15000);
+    }
+}
