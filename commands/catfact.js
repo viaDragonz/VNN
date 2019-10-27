fdcf405bb103df4829ddd21e5a4ea70a26b25d51
toddhowardguild <53285119+toddhowardguild@users.noreply.github.com>
2019-10-27 15:24:39 +1030

    Cat Fact
    This seems like a cat fact. Basically ".fact" with cat facts.

---
+++ commands/catfact.js
@@ -0,0 +1,30 @@
+const config = require("../config.json");
+const Sentry = require('@sentry/node');
+Sentry.init({
+    dsn: `${config.dsn}`
+});
+const fs = require('fs');
+const http = require('http');
+var request = require('request');
+const {
+    get
+} = require("snekfetch");
+const talkedRecently = new Set();
+
+exports.run = (client, message, args) => {
+    if (talkedRecently.has(message.author.id)) {
+        message.channel.send("Wait 15 seconds before trying this again. - " + message.author);
+    } else {
+        try {
+            get('https://meowfacts.herokuapp.com/').then(res => {
+                return message.channel.send(res.body.text);
+            });
+        } catch (err) {
+            return Sentry.captureException(err);
+        }
+        talkedRecently.add(message.author.id);
+        setTimeout(() => {
+            talkedRecently.delete(message.author.id);
+        }, 15000);
+    }
+}
