99452938792a55f05e782ae1c445b70a169b9ff2
toddhowardguild <53285119+toddhowardguild@users.noreply.github.com>
2019-10-27 17:10:14 +1030

    Fixed ishlvrout. Layer this, Jota.
    "Layer" is not a major at this university. 
    
    Fixed some punctuation.

--- commands/ishlvrout.js
+++ commands/ishlvrout.js
@@ -11,7 +11,7 @@ exports.run = (client, message, args) => {
     if (talkedRecently.has(message.author.id)) {
         message.channel.send("Wait 15 seconds before trying this again. - " + message.author);
     } else {
-        message.reply(`not yet... Try again sooner or layer :(`)
+        message.reply(`not yet... Try again sooner or later :(`)
         talkedRecently.add(message.author.id);
         setTimeout(() => {
             talkedRecently.delete(message.author.id);
