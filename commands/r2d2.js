67dbd506bba1f9fdf1130c9df260b34743c5babc
toddhowardguild <53285119+toddhowardguild@users.noreply.github.com>
2019-10-30 09:57:49 +1030

    Quickly threw together .r2d2 as per request of Womble-TC.
    Fuck shit fuck shit

---
+++ commands/r2d2.js
@@ -0,0 +1,19 @@
+const config = require("../config.json");
+const Sentry = require('@sentry/node');
+Sentry.init({
+    dsn: `${config.dsn}`
+});
+const talkedRecently = new Set();
+
+
+exports.run = (client, message, args) => {
+    if (talkedRecently.has(message.author.id)) {
+        message.channel.send("Wait 15 seconds before trying this again. - " + message.author);
+    } else {
+        message.channel.send("Bleep Bloop.")
+        talkedRecently.add(message.author.id);
+        setTimeout(() => {
+            talkedRecently.delete(message.author.id);
+        }, 15000);
+    }
+}
