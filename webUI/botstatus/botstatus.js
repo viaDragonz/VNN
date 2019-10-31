06e2624d375fa735c02957de79e55a02c5d9608e
Jota <jotarjotinha@gmail.com>
2019-10-31 03:46:27 -0300

    Fixed botstatus (IT WORKS!) and fixed moving Engineer when resizing the browser window

--- webUI/botstatus/botstatus.js
+++ webUI/botstatus/botstatus.js
@@ -3,12 +3,15 @@ botStatus.onreadystatechange = function () {
     //if (this.readyState == 4 && this.status == 200) {
     //   document.getElementById('botstatus').innerHTML = botStatus.responseText;
     //}
-    if (botStatus.responseText === `[{ "status":"online","color":"green" }]`) {
+    if (botStatus.responseText === '[{ "status":"online","color":"green" }]') {
         document.getElementById('botstatus').innerHTML = "ONLINE";
+        document.getElementById('botstatus').style.color = "green";
+        document.getElementById('engineer').style.backgroundImage = "url('../images/engineer_on.png')";
     }
-    if (botStatus.responseText === `[{ "status":"offline","color":"red" }]`) {
-        document.getElementById('botstatus').innerHTML = "OFFLINE";
-    } else {
+    if (botStatus.responseText === '[{ "status":"offline","color":"red" }]') {
+        document.getElementById('botstatus').style.color = "red";
+        document.getElementById('engineer').style.backgroundImage = "url('../images/engineer_off.png')";
+        document.getElementById('engineer').style.backgroundSize = "275px";
         document.getElementById('botstatus').innerHTML = "OFFLINE";
     }
 };
