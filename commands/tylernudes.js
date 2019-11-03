94ac3360baadcde3d23a7393012063bc6f3a0ba9
Jota <jotarjotinha@gmail.com>
2019-11-03 05:14:48 -0300

    Fixed the cooldown

--- commands/tylernudes.js
+++ commands/tylernudes.js
@@ -9,12 +9,12 @@ const talkedRecently = new Set();
 
 exports.run = (client, message, args) => {
     if (talkedRecently.has(message.author.id)) {
-        message.channel.send("Wait 15 seconds before trying this again. - " + message.author);
+        message.channel.send("Wait 10 seconds before trying this again. - " + message.author);
     } else {
         message.reply(`Nudes denied. <\:vnnCat2\:468562233174261766> `)
         talkedRecently.add(message.author.id);
         setTimeout(() => {
             talkedRecently.delete(message.author.id);
-        }, 15000);
+        }, 10000);
     }
 }
