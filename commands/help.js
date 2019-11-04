745a754f3b2c6ba3949e101725e7cbf5a1951bc4
Jota <jotarjotinha@gmail.com>
2019-11-04 15:26:06 -0300

    Small

--- commands/help.js
+++ commands/help.js
@@ -4,14 +4,13 @@ Sentry.init({
     dsn: `${config.dsn}`
 });
 
-
 const talkedRecently = new Set();
 
 exports.run = (client, message, args) => {
     if (talkedRecently.has(message.author.id)) {
         message.channel.send("Wait 10 seconds before trying this again. - " + message.author);
     } else {
-        message.reply("All commands/necessary info can be found here: https://github.com/aidenjay345/VNN/blob/master/README.md || And since the bot is on GitHub, you can watch the repo to see every update we push to it.")
+        message.reply("all commands/necessary info can be found here: https://github.com/aidenjay345/VNN/blob/master/README.md || And since the bot is on GitHub, you can watch the repo to see every update we push to it.")
         talkedRecently.add(message.author.id);
         setTimeout(() => {
             talkedRecently.delete(message.author.id);
