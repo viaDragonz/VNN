407bf8919222d30fa7c85280faf4037fda713b58
acfc <admin@unethicalimages.us>
2019-11-18 18:48:59 -0500

    HLVR update

--- commands/ishlvrout.js
+++ commands/ishlvrout.js
@@ -11,7 +11,7 @@ exports.run = (client, message, args) => {
     if (talkedRecently.has(message.author.id)) {
         message.channel.send("Wait 10 seconds before trying this again. - " + message.author);
     } else {
-        message.reply(`not yet... Try again sooner or later :(`)
+        message.reply(`Its real, and its soooo close.`)
         talkedRecently.add(message.author.id);
         setTimeout(() => {
             talkedRecently.delete(message.author.id);
