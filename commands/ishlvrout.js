3afa7d993bcae849a08a33f25b8d9741b4327e66
toddhowardguild <53285119+toddhowardguild@users.noreply.github.com>
2019-11-19 13:36:04 +1030

    HLVR DLC

--- commands/ishlvrout.js
+++ commands/ishlvrout.js
@@ -11,7 +11,7 @@ exports.run = (client, message, args) => {
     if (talkedRecently.has(message.author.id)) {
         message.channel.send("Wait 10 seconds before trying this again. - " + message.author);
     } else {
-        message.reply(`Its real, and its soooo close.`)
+        message.reply(`Not yet, but it's real - and we're almost there. Check https://hlvr.deaths.live/ for a countdown to Thursday, where we'll get more information.`)
         talkedRecently.add(message.author.id);
         setTimeout(() => {
             talkedRecently.delete(message.author.id);
