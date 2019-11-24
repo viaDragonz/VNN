02e604efc8ebc27e58074c50861f296698c028f9
acfc <admin@unethicalimages.us>
2019-11-24 12:46:58 -0500

    Revert "PIN PIN MUST PIN | Formatting"
    This reverts commit 290f10e7b7936e1326e77be9007bf5f4639e47dc.

--- commands/ishlvrout.js
+++ commands/ishlvrout.js
@@ -11,7 +11,7 @@ exports.run = (client, message, args) => {
     if (talkedRecently.has(message.author.id)) {
         message.channel.send("Wait 10 seconds before trying this again. - " + message.author);
     } else {
-        message.reply(`Almost! It's coming out March 2020, check out the trailer and pre-order the game here: https://store.steampowered.com/app/546560/HalfLife_Alyx/`)
+        message.reply(`Almost! It's coming out March 2020, check out the trailer and pre-order the game here: \n https://store.steampowered.com/app/546560/HalfLife_Alyx/`)
         talkedRecently.add(message.author.id);
         setTimeout(() => {
             talkedRecently.delete(message.author.id);
