655604eb7d6c721e3271f48950454db30d09fb89
toddhowardguild <53285119+toddhowardguild@users.noreply.github.com>
2019-11-25 14:30:22 +1030

    Update userinfo

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -16,15 +16,14 @@ exports.run = (client, message, args) => {
         const embed = new client.RichEmbed()
         .setColor("#FFA200")
         .setAuthor(message.author.username)
-        .setTitle("Your informations")
+        .setTitle("User information")
         .setThumbnail(message.author.avatarURL)
-        .addField("Joined at", message.author.joinedAt)
         .addField("Username", `${message.author.username}#${message.author.discriminator}`)
-        .addField("ID", message.author.id)
-        .addField("Created at", message.author.createdAt)
-        .addField("Joined at", message.member.joinedAt)
-        .addField("Bot?", message.author.bot)
-        .setFooter(`Don't let this information distract you from the fact that Tyler missed out on "that thing".`)
+        .addField("User ID", message.author.id)
+        .addField("Created on", message.author.createdAt)
+        .addField("Joined on", message.member.joinedAt)
+        .addField("Is this user a bot?", message.author.bot)
+        .setFooter(`Don't let this information distract you from the fact that Tyler missed out on a free... um... *thing*.`)
         message.channel.sendEmbed(embed);
     }
     
