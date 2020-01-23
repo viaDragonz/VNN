22cb2e8c527e65b508fe819a9a4f80e04c8e3f2b
jota11 <jotarjotinha@gmail.com>
2020-01-23 03:14:59 -0300

    Fish command! Well, not really.

---
+++ commands/fish.js
@@ -0,0 +1,30 @@
+const config = require("../config.json");
+const Sentry = require('@sentry/node');
+Sentry.init({
+    dsn: `${config.dsn}`
+});
+
+const {
+    get
+} = require("snekfetch");
+const talkedRecently = new Set();
+
+exports.run = (client, message, args) => {
+    if (talkedRecently.has(message.author.id)) {
+        message.channel.send("Wait 10 seconds before trying this again. - " + message.author);
+    } else {
+        const fish = [
+            "Fish? Nope.",
+            "Fish? No.",
+            "Fish? Maybe next time.",
+            "No signals of fish.",
+            "Fish? Try again.",
+            "ERROR 404 - FISH WAS NOT FOUND IN THIS DIRECTORY"
+        ];
+        message.channel.send(`${fish[Math.floor(Math.random() * fish.length)]}`)
+        talkedRecently.add(message.author.id);
+        setTimeout(() => {
+            talkedRecently.delete(message.author.id);
+        }, 10000);
+    }
+}
